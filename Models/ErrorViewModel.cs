2fa45dfbe1da47715eb789c76e541244038dda8b
lautiyuke <lautiyuke@gmail.com>
2023-05-04 17:57:22 -0300

    Creo el proyecto y agrego el .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP04_Yukelson_Steimberg.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
