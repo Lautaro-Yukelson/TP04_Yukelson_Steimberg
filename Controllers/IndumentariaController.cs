5761dd1f20e10510889f7d5561862e96f168fcbb
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-05-10 11:59:30 -0300

    Pongo bien los nombres de las imagenes y los cargo en la lista

--- Controllers/IndumentariaController.cs
+++ Controllers/IndumentariaController.cs
@@ -25,15 +25,16 @@ namespace TP04_Yukelson_Steimberg.Controllers
         [HttpPost]
         public IActionResult GuardarIndumentaria(string equipo, string media, string pantalon, string remera)
         {
-            ViewBag.algoCargado = false;
-            if (string.IsNullOrEmpty(media) || !Equipos.ListaMedias.Contains(media)) ViewBag.Error = "El parametro MEDIA ha sido mal cargado";
-            else if (string.IsNullOrEmpty(pantalon) || !Equipos.ListaPantalones.Contains(pantalon)) ViewBag.Error = "El parametro PANTALON ha sido mal cargado";
-            else if (string.IsNullOrEmpty(remera) || !Equipos.ListaRemeras.Contains(remera)) ViewBag.Error = "El parametro REMERA ha sido mal cargado";
-            else
-            {
-                Equipos.EquiposIndumentaria.Add(equipo, new Indumentaria(media, pantalon, remera));
-                Equipos.algoCargado = true;
-                ViewBag.Success = "La indumentaria se ha creado correctamente.";
+            ViewBag.algoCargado = false;{
+                if (string.IsNullOrEmpty(media) || !Equipos.ListaMedias.Contains(media)) ViewBag.Error = "El parametro MEDIA ha sido mal cargado";
+                else if (string.IsNullOrEmpty(pantalon) || !Equipos.ListaPantalones.Contains(pantalon)) ViewBag.Error = "El parametro PANTALON ha sido mal cargado";
+                else if (string.IsNullOrEmpty(remera) || !Equipos.ListaRemeras.Contains(remera)) ViewBag.Error = "El parametro REMERA ha sido mal cargado";
+                else
+                {
+                    Equipos.EquiposIndumentaria.Add(equipo, new Indumentaria(media, pantalon, remera));
+                    Equipos.algoCargado = true;
+                    ViewBag.Success = "La indumentaria se ha creado correctamente.";
+                }
             }
             return View();
         }
