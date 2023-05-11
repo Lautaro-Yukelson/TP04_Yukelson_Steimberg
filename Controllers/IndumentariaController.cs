3db00136eb453622158ebf73fc045442ee9a260f
LAUTARO FERNANDO YUKELSON <47434208@ort>
2023-05-11 15:56:52 -0300

    Subir indumentaria personalizada. Falta redirect

--- Controllers/IndumentariaController.cs
+++ Controllers/IndumentariaController.cs
@@ -1,11 +1,18 @@
+using System.IO;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using TP04_Yukelson_Steimberg.Models;
 
 namespace TP04_Yukelson_Steimberg.Controllers
 {
     public class IndumentariaController : Controller
     {
+        private IWebHostEnvironment Environment;
+
+        public IndumentariaController(IWebHostEnvironment environment){
+            Environment = environment;
+        }
         public IActionResult Index()
         {
             ViewBag.dicEquipos = Equipos.EquiposIndumentaria;
@@ -23,20 +30,35 @@ namespace TP04_Yukelson_Steimberg.Controllers
         }
 
         [HttpPost]
-        public IActionResult GuardarIndumentaria(string equipo, string media, string pantalon, string remera)
+        public IActionResult GuardarIndumentaria(string equipo, string media, string shorts, string remera)
         {
-            ViewBag.algoCargado = false;{
+            ViewBag.algoCargado = false;
             if (string.IsNullOrEmpty(media) || !Equipos.ListaMedias.Contains(media)) ViewBag.Error = "El parametro MEDIA ha sido mal cargado";
-            else if (string.IsNullOrEmpty(pantalon) || !Equipos.ListaShorts.Contains(pantalon)) ViewBag.Error = "El parametro PANTALON ha sido mal cargado";
+            else if (string.IsNullOrEmpty(shorts) || !Equipos.ListaShorts.Contains(shorts)) ViewBag.Error = "El parametro SHORT ha sido mal cargado";
             else if (string.IsNullOrEmpty(remera) || !Equipos.ListaRemeras.Contains(remera)) ViewBag.Error = "El parametro REMERA ha sido mal cargado";
             else
             {
-                Equipos.EquiposIndumentaria.Add(equipo, new Indumentaria(media, pantalon, remera));
+                Equipos.EquiposIndumentaria.Add(equipo, new Indumentaria(media, shorts, remera));
                 Equipos.algoCargado = true;
                 ViewBag.Success = "La indumentaria se ha creado correctamente.";
             }
-            }
             return View();
+        }
+
+        public IActionResult SubirIndumentaria()
+        {
+            ViewBag.algo = "NICE";
+            return View();
+        }
+
+        [HttpPost]
+        public void SubirNuevaIndumentaria(string nombre, string categoria, IFormFile MyFile){
+            if (MyFile.Length>0){
+                string wwwRootLocal = this.Environment.ContentRootPath + @$"\wwwroot\assets\{categoria}\{MyFile.FileName}";
+                using (var stream = System.IO.File.Create(wwwRootLocal)){
+                    MyFile.CopyToAsync(stream);
+                }
+            }
         }
     }
 }
