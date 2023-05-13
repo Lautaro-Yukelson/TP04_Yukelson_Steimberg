84c32bdd89f99fd0d13f1e8e36aef5a57d255dca
lautiyuke <lautiyuke@gmail.com>
2023-05-12 21:40:24 -0300

    Borro el formulario para crear indumentaria y hago las imagenes seleccionables. (EN PROCESO)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,20 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function setearMedia(){
+function setearMedia(event) {
     let imagenClickeada = event.target;
     let idImagen = imagenClickeada.id;
     document.creador.media.value = idImagen;
 }
-
-function setearShort(){
+  
+function setearShort(event) {
     let imagenClickeada = event.target;
     let idImagen = imagenClickeada.id;
     document.creador.shorts.value = idImagen;
 }
-
-function setearRemera(){
+  
+function setearRemera(event) {
     let imagenClickeada = event.target;
     let idImagen = imagenClickeada.id;
     document.creador.remera.value = idImagen;
 }
+  
+function clickImagen() {
+    // Obtener la fila de la imagen clickeada
+    let fila = this.closest("tr");
+    // Obtener todas las imágenes de la fila
+    let imagenesFila = fila.querySelectorAll("img.no-select");
+    // Si la imagen ya está seleccionada, deseleccionarla
+    if (this.classList.contains("select")) {
+        this.classList.remove("select");
+    } else {
+    // Si la imagen no está seleccionada, seleccionarla y deseleccionar
+    // todas las demás imágenes de la misma fila
+    for (let i = 0; i < imagenesFila.length; i++) {
+        if (imagenesFila[i] !== this && imagenesFila[i].classList.contains("select")) {
+            imagenesFila[i].classList.remove("select");
+        }
+      }
+    this.classList.add("select");
+    }
+}
+  
+for (imagen of document.querySelectorAll("img.no-select")) {
+    imagen.addEventListener("click", clickImagen);
+}
+  
+  
